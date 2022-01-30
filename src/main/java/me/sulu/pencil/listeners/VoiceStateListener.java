41b9ba3e7180f79e74c20ba7cf8ec4a023c7a047
sulu5890 <sulu@sulu.me>
2022-01-30 13:53:22 -0600

    fix and improve voice logging

--- src/main/java/me/sulu/pencil/listeners/VoiceStateListener.java
+++ src/main/java/me/sulu/pencil/listeners/VoiceStateListener.java
@@ -2,8 +2,8 @@ package me.sulu.pencil.listeners;
 
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.VoiceStateUpdateEvent;
-import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.entity.RestChannel;
 import discord4j.rest.util.Color;
 import me.sulu.pencil.Pencil;
 import reactor.core.publisher.Mono;
@@ -20,37 +20,44 @@ public class VoiceStateListener {
     long voiceLogId = this.pencil.config().guild(event.getCurrent().getGuildId()).channels().voiceLog();
     if (voiceLogId == 0L) return Mono.empty();
 
-    return event.getCurrent().getGuild()
-      .flatMap(guild -> guild.getChannelById(Snowflake.of(voiceLogId)))
-      .cast(TextChannel.class)
-      .zipWith(event.getCurrent().getUser())
-      .flatMap(tuple -> {
+    final RestChannel logChannel = this.pencil.client().rest().getChannelById(Snowflake.of(voiceLogId));
+
+    return event.getCurrent().getUser()
+      .flatMap(user -> {
         EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder()
-          .author(tuple.getT2().getTag(), null, tuple.getT2().getAvatarUrl())
-          .addField("User Mention", tuple.getT2().getMention(), false)
-          .addField("Channel Mention", tuple.getT2().getMention(), false);
+          .author(user.getTag(), null, user.getAvatarUrl())
+          .addField("User Mention", user.getMention(), false);
 
         if (event.isLeaveEvent()) {
           return event.getOld().get().getChannel()
             .map(old -> builder.title("Left " + old.getName())
               .color(Color.RUST)
-              .build())
-            .flatMap(tuple.getT1()::createMessage);
+              .addField("Channel mention", old.getMention(), false)
+              .build()
+              .asRequest()
+            )
+            .flatMap(logChannel::createMessage);
         }
 
         return event.getCurrent().getChannel()
           .flatMap(current -> {
             if (event.isJoinEvent()) {
-              return tuple.getT1().createMessage(builder.title("Joined " + current.getName())
+              return logChannel.createMessage(builder.title("Joined " + current.getName())
+                .addField("Channel Mention", current.getMention(), false)
                 .color(Color.ENDEAVOUR)
-                .build());
+                .build()
+                .asRequest()
+              );
             } else if (event.isMoveEvent() && event.getOld().isPresent()) {
               return event.getOld().get().getChannel()
                 .map(oldChannel -> builder.title("Moved from " + oldChannel.getName() + " to " + current.getName())
                   .color(Color.MOON_YELLOW)
+                  .addField("New Channel Mention", current.getMention(), false)
                   .addField("Old Channel Mention", oldChannel.getMention(), false)
-                  .build())
-                .flatMap(tuple.getT1()::createMessage);
+                  .build()
+                  .asRequest()
+                )
+                .flatMap(logChannel::createMessage);
             }
 
             return Mono.empty();

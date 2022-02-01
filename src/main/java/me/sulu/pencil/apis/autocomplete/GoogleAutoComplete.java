0770223da8120ae582a2dd85a9c08fec37156adf
sulu5890 <sulu@sulu.me>
2022-01-31 21:31:46 -0600

    fix empty google autocomplete response

--- src/main/java/me/sulu/pencil/apis/autocomplete/GoogleAutoComplete.java
+++ src/main/java/me/sulu/pencil/apis/autocomplete/GoogleAutoComplete.java
@@ -1,15 +1,20 @@
 package me.sulu.pencil.apis.autocomplete;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import discord4j.discordjson.json.ApplicationCommandOptionChoiceData;
 import me.sulu.pencil.Pencil;
 import reactor.core.publisher.Flux;
+import reactor.util.Logger;
+import reactor.util.Loggers;
 
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.Locale;
 
 public class GoogleAutoComplete {
+  private static final Logger LOGGER = Loggers.getLogger(GoogleAutoComplete.class);
   private final Pencil pencil;
 
   public GoogleAutoComplete(final Pencil pencil) {
@@ -26,11 +31,16 @@ public class GoogleAutoComplete {
       .uri(this.url(term))
       .responseContent()
       .aggregate()
-      .asByteArray()
+      .asString()
       .flatMapIterable(data -> {
         try {
-          return this.pencil.xmlMapper().readTree(data).get("CompleteSuggestion");
+          final JsonNode suggestions = this.pencil.xmlMapper().readTree(data).get("CompleteSuggestion");
+          if (suggestions == null) {
+            return Collections.emptyList();
+          }
+          return suggestions;
         } catch (IOException e) {
+          LOGGER.warn("Failed to handle Google autocomplete API response {}", data);
           throw new RuntimeException(e);
         }
       })
